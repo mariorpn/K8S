67cc57c3f1e30c3021959918fdc873fb247e8e78
Mario Neto <mariorpn@yahoo.com.br>
2020-01-26 16:47:12 -0300

    Update k8s_lab_cluster_install.sh

--- k8s_lab_cluster_install.sh
+++ k8s_lab_cluster_install.sh
@@ -22,7 +22,6 @@ hostnamectl set-hotname k8s-master
 kubeadm config images pull
 kubeadm init
 mkdir -p $HOME/.kube
-
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
@@ -30,12 +29,12 @@ echo "source <(kubectl completion bash)" >> ~/.bashrc
 
 # NODE 01
 # CPU: 2 RAM: 1GB
-#hostnamectl set-hotname k8s-node01
-#kubeadm join --token 39c341.a3bc3c4dd49758d5 192.168.10.10:6443 --discovery-token-ca-cert-hash sha256:37092
-#echo "source <(kubectl completion bash)" >> ~/.bashrc
+hostnamectl set-hotname k8s-node01
+kubeadm join --token 39c341.a3bc3c4dd49758d5 192.168.10.10:6443 --discovery-token-ca-cert-hash sha256:37092
+echo "source <(kubectl completion bash)" >> ~/.bashrc
 
 # NODE 02
 # CPU: 2 RAM: 1GB
-#hostnamectl set-hotname k8s-node02
-#kubeadm join --token 39c341.a3bc3c4dd49758d5 192.168.10.10:6443 --discovery-token-ca-cert-hash sha256:37092
-#echo "source <(kubectl completion bash)" >> ~/.bashrc
+hostnamectl set-hotname k8s-node02
+kubeadm join --token 39c341.a3bc3c4dd49758d5 192.168.10.10:6443 --discovery-token-ca-cert-hash sha256:37092
+echo "source <(kubectl completion bash)" >> ~/.bashrc
